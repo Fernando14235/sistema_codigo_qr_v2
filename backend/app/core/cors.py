d7d212042489903ce193b27b4b02dd904fb063aa
Fernando Velasquez <nandix2004@gmail.com>
2025-09-02 07:55:48 -0600

    Eliminar archivos python

--- backend/app/core/cors.py
+++ backend/app/core/cors.py
@@ -4,9 +4,9 @@ def add_cors(app):
     app.add_middleware(
         CORSMiddleware,
         allow_origins=[
-            "sistemacodigoqrv2-production.up.railway.app",      # React web
-            "exquisite-healing-production.up.railway.app",      # React web
-            "*" #para permitir conexiones de cualquier origen de red
+            "sistemacodigoqrv2-production.up.railway.app",
+            "exquisite-healing-production.up.railway.app",  
+            #"*" #para permitir conexiones de cualquier origen de red
         ],
         allow_credentials=True,
         allow_methods=["*"],
