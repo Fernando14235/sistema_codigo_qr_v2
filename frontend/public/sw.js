f31671c71ffc56fd9cb55dffb47cb72b2daf0af6
Fernando Velasquez <nandix2004@gmail.com>
2025-12-09 11:48:15 -0600

    nueva version SW 2.1.2

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,5 +1,5 @@
 // Service Worker para Residencial Access PWA
-const CACHE_NAME = 'residencial-access-v1.1.1';
+const CACHE_NAME = 'residencial-access-v2.1.2';
 const urlsToCache = [
   '/',
   '/index.html',
